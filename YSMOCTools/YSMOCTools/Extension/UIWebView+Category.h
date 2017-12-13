19eac63d77de6ec7ec8b284562328ba5d5b1b39d
duanzengguang <duanzengguang@duanzengguangdeMacBook-Pro.local>
2017-12-13 11:45:54 +0800

    uiwebview user-agent

---
+++ YSMOCTools/YSMOCTools/Extension/UIWebView+Category.h
@@ -0,0 +1,16 @@
+//
+//  UIWebView+Category.h
+//  YSMOCTools
+//
+//  Created by duanzengguang on 2017/12/13.
+//  Copyright © 2017年 忆思梦吧. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIWebView (Category)
+
+// 更新User-Agent
++ (void)appendingUserAgent:(NSString *)str;
+
+@end
