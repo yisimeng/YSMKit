74168356fac589fae295514e0a4a79c479c94bec
duanzengguang <465386131@qq.com>
2018-03-26 10:37:46 +0800

    string extension

---
+++ YSMSwiftTools/YSMSwiftTools/NewShareView.swift
@@ -0,0 +1,21 @@
+//
+//  NewShareView.swift
+//  YSMSwiftTools
+//
+//  Created by duanzengguang on 2018/3/5.
+//  Copyright © 2018年 忆思梦吧. All rights reserved.
+//
+
+import UIKit
+
+class NewShareView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
